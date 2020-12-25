129dc72e433dc6373e58156764107f1fc1717e65
IndraCahya123 <indracahya51@gmail.com>
2020-12-25 20:03:03 +0700

    add an explanation about git-remote

--- index.php
+++ index.php
@@ -14,6 +14,22 @@
         this project will be duplicate to the other place.
 
         in this tutorial we will duplicate this in to github.
+
+        how to duplicate our local project to github, first make new repo in your github account
+        with the same name with your local project name.
+
+        and do not check the initialize README and you will be redirect to the config page in github.
+
+        so there will be several code that github provide.
+
+        the first code is use when we doesn't have any project to duplicate.
+
+        and the second code is use when we already have a project to duplicate to github.
+
+        we just the first and the third line in that code. 
+        (just copy that code in the git bash or command promt)
+
+        and u have a duplicate project in your github account
     </pre>
 </body>
 </html>
