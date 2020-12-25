4ae7c3d4aa276f0c8a222b74ce8d8e187acc1d87
IndraCahya123 <indracahya51@gmail.com>
2020-12-25 19:51:55 +0700

    add file index.html

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Remote</title>
+</head>
+<body>
+    <h1>Hello word</h1>
+
+
+    <pre>
+        this project will be duplicate to the other place.
+
+        in this tutorial we will duplicate this in to github.
+    </pre>
+</body>
+</html>
